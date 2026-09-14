c9df1647cb1071786720500570fa70bf39981b22
Aleks Stansbury <astansbury9@huskers.unl.edu>
2026-09-14 14:43:14 -0500

    Added headers files etc

---
+++ include/stepper.h
@@ -0,0 +1,9 @@
+#ifndef STEPPER_H
+#define STEPPER_H
+
+// Function prototypes for controlling the stepper motor
+void stepperCW(int steps);
+void stepperCCW(int steps);
+void stepperInit();
+
+#endif // STEPPER_H
