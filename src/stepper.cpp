f8733b7f0d05827f73c7dc1737f779605fc39895
Aleks Stansbury <astansbury9@huskers.unl.edu>
2026-09-14 14:51:11 -0500

    Added comments for better documentation

--- src/stepper.cpp
+++ src/stepper.cpp
@@ -1,21 +1,20 @@
 #include "stepper.h"
 #include <Arduino.h>
 
-// Define these per your actual wiring
-#define STEP_PIN 4
-#define DIR_PIN 5
+#define STEP_PIN 4 // stepper pin
+#define DIR_PIN 5  // direction pin
 
 void stepperInit()
 {
-    pinMode(STEP_PIN, OUTPUT);
+    pinMode(STEP_PIN, OUTPUT); // set both pins as output
     pinMode(DIR_PIN, OUTPUT);
 }
 
 void stepperCW(int steps)
 {
-    digitalWrite(DIR_PIN, HIGH);
+    digitalWrite(DIR_PIN, HIGH); // sets the direction the stepper spins in to clockwise
 
-    for (int i = 0; i < steps; i++)
+    for (int i = 0; i < steps; i++) // for each step, set the step pin high so the motor spins and then low to stop
     {
         digitalWrite(STEP_PIN, HIGH);
         delayMicroseconds(1000);
@@ -26,10 +25,11 @@ void stepperCW(int steps)
 
 void stepperCCW(int steps)
 {
-    digitalWrite(DIR_PIN, LOW);
+    digitalWrite(DIR_PIN, LOW); // sets the direction the stepper spins in to counter clockwise
+
     for (int i = 0; i < steps; i++)
     {
-        digitalWrite(STEP_PIN, HIGH);
+        digitalWrite(STEP_PIN, HIGH); // for each step, set the step pin high so the motor spins and then low to stop
         delayMicroseconds(1000);
         digitalWrite(STEP_PIN, LOW);
         delayMicroseconds(1000);
